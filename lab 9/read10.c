9eafed5f5ebfed04bd6896424b6c1c680921b576
d1ndonlymdhe <ayushshrestha8920@protonmail.com>
2023-03-04 19:08:39 +0545

    fix

--- lab 9/read10.c
+++ lab 9/read10.c
@@ -2,11 +2,11 @@
 int main(){
     int num[10];
     printf("Enter 10 numbers \n");
-    for(int i=0;i<9;i++)
+    for(int i=0;i<=9;i++)
     {
         scanf("%d",&num[i]);
     }
-    for(int i=0;i<9;i++){
+    for(int i=0;i<=9;i++){
         printf("%d\n",num[i]);
     }
    return 0;
